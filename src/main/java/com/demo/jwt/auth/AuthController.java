56cf5656d377cf90918ad732e22cbd6fed9cb3d3
Agustin Moldavsky <agusting.moldavsky@gmail.com>
2023-10-27 16:37:20 -0300

    Login and register endpoint configuration

--- src/main/java/com/demo/jwt/auth/AuthController.java
+++ src/main/java/com/demo/jwt/auth/AuthController.java
@@ -1,6 +1,8 @@
 package com.demo.jwt.auth;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,17 +12,24 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
+		
 	
+		private final AuthService authService;
 		
 		@PostMapping(value = "login")
-		public String login() {
-			return "Login from a public endpoint";
+		public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+			
+			return ResponseEntity.ok(authService.login(request));
+			
 		}
 		
 		
 		@PostMapping(value = "register")
-		public String register() {
-			return "Register from a public endpoint";
+		public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+			
+			return ResponseEntity.ok(authService.register(request));
+			
+					
 		}
 	
 }
