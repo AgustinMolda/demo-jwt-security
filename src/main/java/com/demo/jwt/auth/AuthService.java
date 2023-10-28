8accf3736f1887870d932e9d74eeabc5bd57c682
Agustin Moldavsky <agusting.moldavsky@gmail.com>
2023-10-28 01:11:07 -0300

    Application working 100%!

--- src/main/java/com/demo/jwt/auth/AuthService.java
+++ src/main/java/com/demo/jwt/auth/AuthService.java
@@ -1,5 +1,9 @@
 package com.demo.jwt.auth;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.demo.jwt.auth.user.Role;
@@ -13,17 +17,25 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthService {
 	
+	
 	private final UserRepository userRepository;
 	private final JwtService jwtService;
+	private final AuthenticationManager authenticationManager;
+	private final PasswordEncoder passwordEncoder;
 	
 	public AuthResponse login (LoginRequest request){
-		return null;
+		authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+		UserDetails user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+		String token = jwtService.getToken(user);
+		return AuthResponse.builder()
+				.token(token)
+				.build();
 	}
 	
 	public AuthResponse register (RegisterRequest request){
 		User user = User.builder()
 				.username(request.getUsername())
-				.password(request.getPassword())
+				.password(passwordEncoder.encode(request.getPassword()))
 				.firstname(request.getFirstname())
 				.lastname(request.getLastname())
 				.country(request.getCountry())
